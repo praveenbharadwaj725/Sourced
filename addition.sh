16a49d8e5b37525e1c8ed5ce949a567461dbe18f
pmbharad <praveenmohan.bharadwaj@microfocus.com>
2019-08-13 11:17:08 +0530

    Added the last line

--- addition.sh
+++ addition.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 echo "This script performs the addition of two numbers"
 TOTAL=$(($N1 + $N2))
-echo "TOTAL of the two numbers is $TOTAL"
+echo "Total of the two numbers is $TOTAL"
+echo "END OF EXECUTION"
 
